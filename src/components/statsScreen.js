1b01efdd61840026ff31f4ce95e110d714c331c2
Jakob molin <molin.jakob@gmail.com>
2020-12-30 15:08:43 +0100

    added top 10 different skills

--- src/components/statsScreen.js
+++ src/components/statsScreen.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, ScrollView, Image, StyleSheet, TouchableOpacity, Alert, Dimensions } from 'react-native';
 import axios from 'react-native-axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Table, Row, Col, TableWrapper } from 'react-native-table-component';
+import { Card, ListItem } from 'react-native-elements'
 
 const allStatsKey = 'allStats'
 const allNamesKey = 'allNames'
 const colWidth = 130
+const windowWidth = Dimensions.get('window').width;
 import pageStyles from '../style/basicStyle';
 
 class StatsScreen extends Component {
@@ -19,6 +21,11 @@ class StatsScreen extends Component {
             nameList: [],
             filteredNameList: [],
 
+            topTotalPoints: [],
+            topTotalAces: [],
+            topTotalKills: [],
+            topTotalBlocks: [],
+
             tableheader: ['', 'Total', 'Serve', 'Reception', 'Attack', 'Block'],
             tableHead: ['Nr', 'Team', 'Mat', 'Set', 'Pts', 'BP', 'W-L', 'Tot', 'Err', '!', '-', '+', 'OVP', 'Ace', 'Tot', 'Err', 'OVP', 'Pos %', 'Perf %', 'Tot', 'Err', 'Block', 'Perf', 'Perf %', 'Eff %', 'Tot', 'Err', 'Perf %', 'Points'],
             widthMainHead: [30, 100, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 55, 55, 55, 40, 55, 45, 60, 65, 45, 45, 60, 55],
@@ -30,17 +37,32 @@ class StatsScreen extends Component {
         let playerList = []
         let nameList = []
         try {
-            console.log("Start")
             playerList = JSON.parse(await AsyncStorage.getItem(allStatsKey))
             nameList = JSON.parse(await AsyncStorage.getItem(allNamesKey))
-            console.log("Done")
         } catch (e) {
             console.log(e)
         }
-        this.setState({ filteredPlayers: playerList.slice(0,30), filteredNameList: nameList.slice(0,30) })
+        this.setState({ filteredPlayers: playerList.slice(0, 30), filteredNameList: nameList.slice(0, 30) })
 
-        this.setState({ filteredPlayers: playerList, filteredNameList: nameList })
+        // this.setState({ filteredPlayers: playerList, filteredNameList: nameList })
+        this.setTop()
         this.setData()
+    }
+    async setTop() {
+        const playerListTotalPoints = await this.makeRequest('PointsTot_ForAllPlayerStats DESC', '174')
+        const playerListTotalAces = await this.makeRequest('ServeWin DESC', '174')
+        const playerListTotalKills = await this.makeRequest('SpikeWin DESC', '174')
+        const playerListTotalBlocks = await this.makeRequest('BlockWin DESC', '174')
+        this.setState({ topTotalPoints: playerListTotalPoints, topTotalAces: playerListTotalAces, topTotalKills: playerListTotalKills, topTotalBlocks: playerListTotalBlocks })
+    }
+    async makeRequest(sortExpression, compID) {
+        let playerList = []
+        await axios.post('http://svbf-web.dataproject.com/Statistics_AllPlayers.aspx/GetData', { "startIndex": 0, "maximumRows": 10, "sortExpressions": sortExpression, "filterExpressions": [], "compID": compID, "phaseID": "266", "playerSearchByName": "" }).then(function (response) {
+            for (let i = 0; i < response.data.d.length; i++) {
+                playerList.push(this.extractNameAndStats(response.data.d[i]))
+            }
+        }.bind(this));
+        return playerList
     }
     async setData() {
         let playerList = []
@@ -60,7 +82,7 @@ class StatsScreen extends Component {
         } catch (e) {
             console.log(e)
         }
-        this.setState({ allPlayers: playerList, filteredPlayers: playerList, filteredNameList: nameList, nameList: nameList })
+        this.setState({ allPlayers: playerList, filteredPlayers: playerList.slice(0, 30), filteredNameList: nameList.slice(0, 30), nameList: nameList })
     }
     formatName(name, surname) {
         let newName = name + ' ' + surname
@@ -76,6 +98,16 @@ class StatsScreen extends Component {
             }
         }
         return newName
+    }
+    extractNameAndStats(data) {
+        return {
+            name: data.Name + ' ' + data.Surname,
+            team: data.Team,
+            totalPoints: data.PointsTot_ForAllPlayerStats,
+            totalKills: data.SpikeWin,
+            totalBlocks: data.BlockWin,
+            totalAces: data.ServeWin,
+        }
     }
     extractData(data) {
         const totalServ = data.ServeErr + data.ServeWin + data.ServeMinus + data.ServePlus + data.ServeHP + data.ServeEx
@@ -114,56 +146,92 @@ class StatsScreen extends Component {
             data.BlockWin,
         ]
     }
+    renderTopContent(title, category, pointName, pointNameDescription) {
+        return (
+            <Card>
+                <Card.Title>{title}</Card.Title>
+                <Card.Divider />
+                {
+                    this.state.[category].map((player, index) => {
+                        return (
+                            <View key={index} style={{ marginTop: 10 }}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Text style={{ marginRight: 'auto' }}>{index + 1}. {player.name} ({player.team}) </Text>
+                                    <Text style={{ marginLeft: 'auto', fontWeight: 'bold' }}>{player.[pointName]}  </Text>
+                                    <Text>{pointNameDescription}</Text>
+                                </View>
+                            </View>
+                        )
+                    })
+                }
+            </Card >
+        )
+    }
+    renderTop() {
+        return (
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={true}>
+                <View style={{ width: windowWidth }}>
+                    {this.renderTopContent('Most points', 'topTotalPoints', 'totalPoints', 'points')}
+                </View>
+                <View style={{ width: windowWidth }}>
+                    {this.renderTopContent('Most aces', 'topTotalAces', 'totalAces', 'aces')}
+                </View>
+                <View style={{ width: windowWidth }}>
+                    {this.renderTopContent('Most kills', 'topTotalKills', 'totalKills', 'kills')}
+                </View>
+                <View style={{ width: windowWidth }}>
+                    {this.renderTopContent('Most blocks', 'topTotalBlocks', 'totalBlocks', 'blocks')}
+                </View>
+            </ScrollView>
+        )
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <TableWrapper style={{ flexDirection: 'row' }}>
-                    <Table borderStyle={{ borderWidth: 1, borderColor: '#f1f8ff' }} >
-                        <Row data={['']} widthArr={[colWidth]} textStyle={styles.header} />
-                        <Row data={['Name']} widthArr={[colWidth]} textStyle={pageStyles.tableText} />
-                    </Table>
-                    <ScrollView style={styles.dataWrapper, { flexDirection: 'row' }} horizontal={true} ref={'topScroll'}>
+            <ScrollView style={styles.container} stickyHeaderIndices={[1]}>
+                {this.renderTop()}
+                <TableWrapper style={{ flexDirection: 'row', backgroundColor: 'white' }}>
+                    <ScrollView style={styles.dataWrapper, { flexDirection: 'row' }} horizontal={true} ref={'topScroll'} showsHorizontalScrollIndicator={false}>
+                        <Table borderStyle={{ borderWidth: 1, borderColor: '#f1f8ff' }} >
+                            <Row data={['']} widthArr={[colWidth]} textStyle={styles.header} />
+                            <Row data={['Name']} widthArr={[colWidth]} textStyle={pageStyles.tableText} />
+                        </Table>
                         <Table borderStyle={pageStyles.borderStyle}>
                             <Row data={this.state.tableheader} widthArr={this.state.widthHeader} textStyle={styles.header} />
                             <Row data={this.state.tableHead} widthArr={this.state.widthMainHead} textStyle={pageStyles.tableText} />
                         </Table>
                     </ScrollView>
                 </TableWrapper>
-                <ScrollView horizontal={false}>
-                    <View>
-                        <TableWrapper style={{ flexDirection: 'row' }}>
-                            <Table borderStyle={{ borderWidth: 1, borderColor: '#f1f8ff' }} >
-                                {/* <Row data={['']} widthArr={[colWidth]} textStyle={styles.header} />
-                                <Row data={['Name']} widthArr={[colWidth]} textStyle={pageStyles.tableText} /> */}
-                                <Col
-                                    data={this.state.filteredNameList}
-                                    width={colWidth}
-                                    textStyle={pageStyles.tableText}
-                                />
-                            </Table>
-                            <ScrollView style={styles.dataWrapper, { flexDirection: 'row' }} horizontal={true} ref={'bottomScroll'}
-                                onScroll={(e) => this.refs.topScroll.scrollTo({ x: e.nativeEvent.contentOffset.x, animated: false })}
-                            >
-                                <Table borderStyle={pageStyles.borderStyle}>
-                                    {/* <Row data={this.state.tableheader} widthArr={this.state.widthHeader} textStyle={styles.header} />
+                <View>
+                    <TableWrapper style={{ flexDirection: 'row' }}>
+                        <Table borderStyle={{ borderWidth: 1, borderColor: '#f1f8ff' }} >
+                            <Col
+                                data={this.state.filteredNameList}
+                                width={colWidth}
+                                textStyle={pageStyles.tableText}
+                            />
+                        </Table>
+                        <ScrollView style={styles.dataWrapper, { flexDirection: 'row' }} horizontal={true} ref={'bottomScroll'}
+                            onScroll={(e) => this.refs.topScroll.scrollTo({ x: e.nativeEvent.contentOffset.x, animated: false })}
+                        >
+                            <Table borderStyle={pageStyles.borderStyle}>
+                                {/* <Row data={this.state.tableheader} widthArr={this.state.widthHeader} textStyle={styles.header} />
                                     <Row data={this.state.tableHead} widthArr={this.state.widthMainHead} textStyle={pageStyles.tableText} /> */}
-                                    {
-                                        this.state.filteredPlayers.map((rowData, index) => (
-                                            <Row
-                                                key={index}
-                                                data={rowData}
-                                                widthArr={this.state.widthMainHead}
-                                                style={[styles.row, index % 2 && pageStyles.tableBackgroundColor]}
-                                                textStyle={pageStyles.tableText}
-                                            />
-                                        ))
-                                    }
-                                </Table>
-                            </ScrollView>
-                        </TableWrapper>
-                    </View>
-                </ScrollView>
-            </View >
+                                {
+                                    this.state.filteredPlayers.map((rowData, index) => (
+                                        <Row
+                                            key={index}
+                                            data={rowData}
+                                            widthArr={this.state.widthMainHead}
+                                            style={[styles.row, index % 2 && pageStyles.tableBackgroundColor]}
+                                            textStyle={pageStyles.tableText}
+                                        />
+                                    ))
+                                }
+                            </Table>
+                        </ScrollView>
+                    </TableWrapper>
+                </View>
+            </ScrollView >
         )
     }
 }
